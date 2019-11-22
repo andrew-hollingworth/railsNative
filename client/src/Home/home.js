178533342912cecfff33b67cf68f9f9a73147000
Andrew Hollingworth <andrew.hollingworth@gmail.com>
2019-11-22 16:34:51 -0500

    Basic styling of items screen

--- client/src/Home/home.js
+++ client/src/Home/home.js
@@ -1,28 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, Text, SafeAreaView } from 'react-native'
 import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const HomeScreen = (props) => {
   const { navigate } = props.navigation;
   return (
-    <>
-      <Button
-        title="Go to Form"
-        onPress={() => navigate('Form', { name: 'Form' })}
-      />
-      <Button
-        title="Profile"
-        onPress={() => navigate('Profile', { name: 'ProfileScreen' })}
-      />
-      <Button
-        title="Wishlist"
-        onPress={() => navigate('WishlistScreen', { name: 'WishlistScreen' })}
-      />
-      <Button
-        title="Items"
-        onPress={() => navigate('ItemScreen', { name: 'ItemScreen' })}
-      />
-    </>
+    <SafeAreaView>
+      <Text>WELCOME TO THE HOMEPAGE</Text>
+    </SafeAreaView>
   );
 }
 
