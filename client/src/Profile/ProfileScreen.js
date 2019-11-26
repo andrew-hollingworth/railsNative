384e013cdc839e331c2fed47b77b1bf46a67ee11
Andrew Hollingworth <andrew.hollingworth@gmail.com>
2019-11-26 10:00:59 -0500

    Add icons to menu. Initial pass at formatting Wishlist page, currently pulls in all items.

--- client/src/Profile/ProfileScreen.js
+++ client/src/Profile/ProfileScreen.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { SafeAreaView, View, Text } from 'react-native'
 
 const ProfileScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Profile goes here!</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- client/src/Wishlist/WishlistScreen.js
+++ client/src/Wishlist/WishlistScreen.js
@@ -1,11 +1,82 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, Button } from 'react-native';
+import { allItem, oneItem } from '../../services/api-helper';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    height: 30,
+    paddingBottom: 40,
+  },
+  itemCard: {
+    display: 'flex',
+    alignSelf: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 17,
+    width: '90%',
+  },
+  itemContainer: {
+    justifyContent: "center",
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  itemDesc: {
+    fontStyle: 'italic',
+  },
+  itemPrice: {
+    textAlign: 'right',
+    paddingRight: 10,
+  },
+});
 
 const WishlistScreen = () => {
+  const [item, setItem] = React.useState({
+  })
+  const [run, setRun] = React.useState({
+    value: true
+  })
+  const getOneItem = async () => {
+    const item = await oneItem();
+    setItem(item)
+  }
+  let list = list = item.item && item.item.map((ele, i) => {
+    return (
+      <View style={styles.itemCard} key={i}>
+        <Text style={styles.itemTitle}>{ele.title}</Text>
+        <Text style={styles.itemDesc}>{ele.description}</Text>
+        <Text style={styles.itemPrice}>${ele.price}0</Text>
+        <Button title='Add to Cart'></Button>
+        <Button color='grey' title='Remove from Wishlist'></Button>
+      </View>
+    )
+  });
+
+  const getAllItem = async () => {
+    const item = await allItem();
+    setItem({ item: item })
+  }
+
+  React.useEffect(() => {
+    if (run.value === true) {
+      getAllItem();
+      setRun({ value: false });
+    }
+  })
   return (
-    <View>
-      <Text>Wishlists go here!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View><Text style={styles.header}>Items</Text></View>
+      <ScrollView contentContainerStyle={styles.itemContainer}>
+        <View>{list}</View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
